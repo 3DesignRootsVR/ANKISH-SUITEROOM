6875fbc0b67fa8865536825d911829a43bb30712
3DesignRootsVR <3designrootscreativecloud@gmail.com>
2026-04-17 17:17:45 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,203 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-suite-room",
+      "name": "SUITE ROOM",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.599813087456484,
+          "pitch": -0.026939968446070495,
+          "rotation": 0,
+          "target": "3-wardrobe"
+        },
+        {
+          "yaw": -0.6070852574004295,
+          "pitch": -0.04014133257303243,
+          "rotation": 0,
+          "target": "2-dressing-table"
+        },
+        {
+          "yaw": 1.5079054478459302,
+          "pitch": 0.07710006330390229,
+          "rotation": 0,
+          "target": "1-walk-in-wardrobe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-walk-in-wardrobe",
+      "name": "WALK-IN WARDROBE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0219064704688883,
+          "pitch": 0.0739664494154546,
+          "rotation": 0,
+          "target": "0-suite-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dressing-table",
+      "name": "DRESSING TABLE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0974523849035833,
+          "pitch": 0.2786093869268367,
+          "rotation": 0,
+          "target": "0-suite-room"
+        },
+        {
+          "yaw": 1.4043967370764214,
+          "pitch": 0.05952369201046537,
+          "rotation": 0,
+          "target": "3-wardrobe"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-wardrobe",
+      "name": "WARDROBE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.015542860102161882,
+          "pitch": 0.15635492159799114,
+          "rotation": 0,
+          "target": "2-dressing-table"
+        },
+        {
+          "yaw": -2.6516288263786745,
+          "pitch": 0.1071656673865764,
+          "rotation": 1.5707963267948966,
+          "target": "0-suite-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "ANKISH-SUITE ROOM",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
